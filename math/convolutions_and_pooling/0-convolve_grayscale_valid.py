c1210be8aa18fcfcf2e7669c156a00bfbbb558ef
Garangbse <g.buke@alustudent.com>
2026-07-13 15:34:08 +0200

    updating 0

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
-"""Performs a valid convolution on grayscale images."""
+"""Module for performing a valid convolution on grayscale images."""
 
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
     """
-    Performs a valid convolution on grayscale images.
+    Perform a valid convolution on grayscale images.
+
+    A valid convolution applies the kernel only where it completely
+    overlaps the image. No padding is added to the images.
 
     Args:
-        images: numpy.ndarray of shape (m, h, w)
-        kernel: numpy.ndarray of shape (kh, kw)
+        images (numpy.ndarray): Array of shape (m, h, w) containing
+            multiple grayscale images.
+            - m: number of images
+            - h: image height
+            - w: image width
+        kernel (numpy.ndarray): Array of shape (kh, kw) containing
+            the convolution kernel.
+            - kh: kernel height
+            - kw: kernel width
 
     Returns:
-        numpy.ndarray containing the convolved images.
+        numpy.ndarray: Array containing the convolved images with shape
+        (m, h - kh + 1, w - kw + 1).
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    output_h = h - kh + 1
-    output_w = w - kw + 1
+    output_height = h - kh + 1
+    output_width = w - kw + 1
 
-    output = np.zeros((m, output_h, output_w))
+    output = np.zeros((m, output_height, output_width))
 
-    for i in range(output_h):
-        for j in range(output_w):
-            patch = images[:, i:i + kh, j:j + kw]
-            output[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
+    for i in range(output_height):
+        for j in range(output_width):
+            region = images[:, i:i + kh, j:j + kw]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
     return output
